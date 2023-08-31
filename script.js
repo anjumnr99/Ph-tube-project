7889cae5fd45dbe1badd01dcc9dd05c931efb175
anjumnr99 <anjumanowara99@gmail.com>
2023-08-31 22:00:07 +0600

    nav category completed

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const loadAllCategory = async () => {
+    const res = await fetch ('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    displayCategory(data);
+};
+
+// Show all Category
+
+const displayCategory = (data) => {
+
+    const categoryContainer = document.getElementById('category-container');
+
+    data?.data.forEach(element => {
+        console.log(element.category);
+
+       let btn = document.createElement('button');
+       btn.innerHTML = ` 
+       
+       <input type="radio" aria-label="${element.category}" class="btn px-8" />
+       `;
+
+       categoryContainer.appendChild(btn);
+
+    });
+}
+
+loadAllCategory();
