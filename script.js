cfc2aea867dd98cd5c3635232079979b5fbe9a24
anjumnr99 <anjumanowara99@gmail.com>
2023-09-02 00:55:28 +0600

    responsive done

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ let navDiv = document.createElement('div');
 navDiv.classList = `navbar bg-base-100 mt-12`;
 navDiv.innerHTML = `  
     <div class="navbar-start">
-            <a href="index.html" class="flex gap-2 ">
+            <a href="index.html" class="flex gap-1 ">
                 <img src="./logo.svg" alt="">
-                <p class="text-4xl tex font-bold"><span class="text-[#FF1F3D]">PH</span> Tube</p>
+                <p class="text-xl md:text-4xl lg:text-4xl tex font-bold"><span class="text-[#FF1F3D]">PH</span> Tube</p>
             </a>
         </div>
-        <div class="navbar-center hidden lg:flex">
-            <button  id="sort-btn" class="btn rounded-lg">Sort by view</button>
+
+        
+        <div class="lg:navbar-center sm:navbar-end">
+            <button  id="sort-btn" class="btn text-base rounded-lg normal-case">Sort by view</button>
         </div>
         <div class="navbar-end">
-            <button onclick="window.location.href='blog-qa.html'" class="btn rounded-lg hover:bg-[#e4223c] bg-[#FF1F3D] text-white"></a>Blog</button>
+            <button onclick="window.location.href='blog-qa.html'" class="btn rounded-lg normal-case hover:bg-[#e4223c] bg-[#FF1F3D] text-white"></a>Blog</button>
         </div>
     `;
 navContainer.appendChild(navDiv);  
@@ -40,7 +42,7 @@ const displayCategory = (data) => {
         let btn = document.createElement('button');
         btn.innerHTML = ` 
        
-       <input onclick = "loadCategoryDetails(${element.category_id})" type="radio" aria-label="${element.category}" class="btn px-8 rounded-lg" />
+       <input onclick = "loadCategoryDetails(${element.category_id})" type="radio" aria-label="${element.category}" class="btn text-lg lg:text-xl md:text-xl normal-case px-8 rounded-lg" />
        `;
 
         categoryContainer.appendChild(btn);
@@ -63,9 +65,6 @@ const loadCategoryDetails = async (categoryId) => {
         
     
          displayDetailsOfCategory(defaultData);
-   
-    
-   
  
 };
 
@@ -121,7 +120,7 @@ const displayDetailsOfCategory = (details) => {
 
     }) : noDataContainer.innerHTML =
     ` <img class="w-20" src="./nodata.svg" alt="">
-    <h1 class="text-3xl text-center font-bold">Oops!! Sorry, There is no <br> content here</h1>` ;
+    <h1 class="text-3xl text-center mb-16 font-bold">Oops!! Sorry, There is no <br> content here</h1>` ;
     
     isSort = false;
 };
